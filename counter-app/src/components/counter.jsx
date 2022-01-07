485f77e95bf658ad84092cbbc15c0143ec55bf12
YWU99u <yuqi14@ualberta.ca>
2022-01-07 09:00:23 -0700

    till 1:08:04. Learnt materials: attributes, lists, styles, className, if loop, for loop, onClick handler

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,14 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends React.Component {
-    render() { 
-        return( 
-        <div>
-            <h1>Hello World</h1>
-            <button>Increment</button>
-        </div>
-        );
-    }
+  state = {
+    count: 0,
+
+    // imageUrl: "https://picsum.photos/200",
+  };
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+    //obj.method(); We can use 'this'
+    //function(); Cannot use 'this', 'this' will return undefine
+  };
+  render() {
+    return (
+      <div>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+
+        <span className={this.get_Badge_Classes()}>{this.formatCount()}</span>
+
+        {/* This classname is bootstrap m-2: margin 2 */}
+
+        {/* In between the curly bracket, there can be any expression, including functions. Render value dynamically */}
+
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  get_Badge_Classes() {
+    let classes = "badge m-2 badge-";
+
+    classes += this.state.count === 0 ? "warning" : "primary";
+
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
